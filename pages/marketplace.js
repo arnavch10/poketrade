ee4536393c02743a6a4e07f2b0ae9eb7e08a8175
hipika <arnavchoudhary53@gmail.com>
2026-04-28 16:09:32 -0400

    added navbar to markplace page

--- pages/marketplace.js
+++ pages/marketplace.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useStateContext } from '@/context/StateContext';
+import CardGrid from '@/components/Dashboard/CardGrid';
+import MarketplaceNavbar from '@/components/Dashboard/MarketplaceNavbar';
+import { useState } from 'react';
 
 const Marketplace = () => {
+
+    // cards
+    const [cards, setCards] = useState([]);
+
   return (
-    <div>marketplace</div>
+    <>
+        <MarketplaceNavbar />
+        <div>marketplace</div>
+        <CardGrid  cards={cards} />
+    </>
+    
   )
 }
 
