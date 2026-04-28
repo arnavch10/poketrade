91532aa0c5689dd12572412d7412ce0d2d0f4dd5
hipika <arnavchoudhary53@gmail.com>
2026-04-27 23:40:21 -0400

    add grid layout for cards component

---
+++ components/Dashboard/CardGrid.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from "styled-components";
+import Card from "./Card";
+
+
+
+const CardGrid = ({ cards = [] }) => {
+  return (
+    <GridWrapper>
+      <Grid>
+        {cards.map((card) => (
+          <Card key={card.id} card={card} />
+        ))}
+      </Grid>
+    </GridWrapper>
+  );
+};
+
+export default CardGrid;
+
+
+
+const GridWrapper = styled.div`
+  width: 100%;
+  padding: 2rem 4rem;
+  display: flex;
+  justify-content: center;
+`;
+
+const Grid = styled.div`
+  width: 100%;
+  max-width: 1400px;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
+  gap: 2rem;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fill, minmax(180px, 1fr));
+    gap: 1.5rem;
+  }
+`;
