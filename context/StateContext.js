9f2bcf6dc9b2bfd457a87380ffe12069b1d0d7ca
hipika <arnavchoudhary53@gmail.com>
2026-04-29 00:36:53 -0400

    updated statecontext

--- context/StateContext.js
+++ context/StateContext.js
@@ -1,10 +1,9 @@
 import { createContext, use, useContext, useState } from "react";
 import { ethers } from "ethers";
-
+import ABI from "../public/abi/PokeContract.json";
 const StateContext = createContext();
 
-const CONTRACT_ADDRESS = "";
-const ABI = [];
+const CONTRACT_ADDRESS = "0x23EcD1cb9e30Be4E14282Db0D2385Dd3f5549299";
 
 export const StateContextProvider = ({ children }) => {
   const [address, setAddress] = useState("");
@@ -20,15 +19,17 @@ export const StateContextProvider = ({ children }) => {
     if (!window.ethereum) return alert("Install MetaMask");
 
     const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = await provider.getSigner();
     const accounts = await provider.send("eth_requestAccounts", []);
     setAddress(accounts[0]);
 
-    const contracts = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider.getSigner());
+    const contracts = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
     setContract(contracts);
+    console.log(contract);
   };
 
   return (
-    <StateContext.Provider value={{ address, connectWallet, contract }}>
+    <StateContext.Provider value={{ address, connectWallet, contract, setAddress, setContract }}>
       {children}
     </StateContext.Provider>
   );
